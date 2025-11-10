4389379d0bf9bcea213bc85d01541ea1b2f13af1
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-11-10 14:42:46 +0600

    Remove some comment line

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,25 +1,4 @@
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: "email-password-auth-9b5e5.firebaseapp.com",
-//   projectId: "email-password-auth-9b5e5",
-//   storageBucket: "email-password-auth-9b5e5.firebasestorage.app",
-//   messagingSenderId: "931523504775",
-//   appId: "1:931523504775:web:dda2c75fdfc65e4ed76622"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export default app;
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
