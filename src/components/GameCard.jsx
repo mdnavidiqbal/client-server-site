132057fa8c9eabc845762280ad999820e156005d
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-11-13 02:21:13 +0600

    Change some portion of code

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { Link } from "react-router";
+// import { FaStar } from "react-icons/fa";
+
+// export default function GameCard({ game }) {
+//   return (
+//     <div key={game.id} className="card shadow-lg">
+//       <figure>
+//         <img
+//           src={game.coverPhoto}
+//           alt={game.title}
+//           className="w-full h-48 object-cover"
+//         />
+//       </figure>
+//       <div className="card-body">
+//         <h3 className="card-title">{game.title}</h3>
+//         <div className="flex justify-between items-center gap-2 text-sm text-gray-700">
+//           <p>{game.category}</p>
+//           Rating: <span className="flex items-center gap-1 text-yellow-400">
+//             {game.ratings}
+//             <FaStar className="text-yellow-400" />
+//           </span>
+//         </div>
+//         <div className="card-actions mt-2 ">
+//           <Link to={`/gamedetails/${game.id}`} className="btn btn-primary bg-[#064e3b] text-white">
+//             Details
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
